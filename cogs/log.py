660f35669cf47bd904ff8109c1b1cc020b59bd7c
AdilJafar <64605907+xcyraxx@users.noreply.github.com>
2021-08-29 16:17:42 +0530

    Add files via upload

---
+++ cogs/log.py
@@ -0,0 +1,115 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Cog
+from discord import Embed, Forbidden
+from datetime import datetime
+import discord
+from discord.utils import get
+
+class Log(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @Cog.listener()
+    async def on_ready(self):
+        print('logs up!')
+        self.channel=self.bot.get_channel(869849124537778214)
+        
+    
+    @Cog.listener()
+    async def on_user_update(self, before, after):
+        if before.name != after.name:
+            embed = Embed(title="Member Update",
+             description=f"{after.name}#{after.discriminator}'s Username was changed",
+              color=discord.Color.from_rgb(73, 131, 179),
+               timestamp = datetime.utcnow())
+
+            fields = [("Before", before.name, False),
+                      ("After", after.name, False)]
+
+            for name, value, inline in fields:
+                embed.add_field(name=name, value=value, inline=inline)
+            await self.channel.send(embed=embed)
+
+        if before.avatar_url != after.avatar_url:
+            embed = Embed(title="Member Update",
+            description=f"{after.name}#{after.discriminator} changed their avatar (New Avatar Below)",
+            color=discord.Color.from_rgb(73, 131, 179),
+            timestamp = datetime.utcnow())
+            embed.set_thumbnail(url=before.avatar_url)
+            embed.set_image(url=after.avatar_url)
+
+            await self.channel.send(embed=embed)
+
+    @Cog.listener()
+    async def on_member_update(self, before, after):
+        if before.display_name != after.display_name:
+            embed = Embed(title="Member Update",
+             description="Nickname was changed",
+              color=discord.Color.from_rgb(73, 131, 179),
+               timestamp = datetime.utcnow())
+
+            fields = [("Before", before.display_name, False),
+                      ("After", after.display_name, False)]
+
+            for name, value, inline in fields:
+                embed.add_field(name=name, value=value, inline=inline)
+            await self.channel.send(embed=embed)
+
+        elif before.roles != after.roles:
+            embed = Embed(title="Member Update",
+             description=f"{after.name}#{after.discriminator}'s Roles were updated",
+              color=discord.Color.from_rgb(73, 131, 179),
+               timestamp = datetime.utcnow())
+            
+            fields = [("Before", ", ".join([r.mention for r in before.roles]), False),
+                      ("After", ", ".join([r.mention for r in after.roles]), False)]
+
+            for name, value, inline in fields:
+                embed.add_field(name=name, value=value, inline=inline)
+            await self.channel.send(embed=embed)
+
+    @Cog.listener()
+    async def on_message_edit(self, before, after):
+        if not after.author.bot:
+            if before.content != after.content:
+                embed = Embed(title=f"Message Edited in #{after.channel}",
+             description=f"**Before:** {before.content} \n **After: ** {after.content}",
+             color=discord.Color.from_rgb(73, 131, 179),
+               timestamp = datetime.utcnow())
+                embed.set_author(name=after.author.name, icon_url=after.author.avatar_url)
+
+                await self.channel.send(embed=embed)
+
+    @Cog.listener()
+    async def on_message_delete(self, message):
+        if not message.author.bot:
+            embed = Embed(title=f"Message Deleted in #{message.channel}",
+             description=f"{message.content}",
+             color=discord.Color.from_rgb(73, 131, 179),
+               timestamp = datetime.utcnow())
+            embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
+            await self.channel.send(embed=embed)   
+
+        
+    @Cog.listener()
+    async def on_guild_channel_delete(self, channel):
+        embed = Embed(title=f"Voice Channel deleted",
+             description=f"**Name: **{channel.name}\n **Category: **{channel.category}",
+             color=discord.Color.from_rgb(73, 131, 179),
+               timestamp = datetime.utcnow())
+        await self.channel.send(embed=embed) 
+
+
+    @Cog.listener()
+    async def on_guild_channel_create(self, channel):
+        embed = Embed(title=f"Voice Channel created",
+             description=f"**Name: **{channel.name}\n **Category: **{channel.category}\n\n**Role override for @everyone**\n",
+             color=discord.Color.from_rgb(73, 131, 179),
+               timestamp = datetime.utcnow())
+        print(channel.overwrites_for(self.everyone))
+        await self.channel.send(embed=embed) 
+            
+
+def setup(bot):
+    bot.add_cog(Log(bot))
