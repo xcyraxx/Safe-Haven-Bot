01cf89414522aa011dc4fc2ad0f4c3140c66062f
AdilJafar <64605907+xcyraxx@users.noreply.github.com>
2021-08-30 22:01:00 +0530

    music cog

--- main.py
+++ main.py
@@ -427,4 +427,5 @@ async def embed(ctx, titlee, *, descriptions):
 
 client.load_extension("cogs.log")
 client.load_extension("cogs.welcome")
+client.load_extension("cogs.music")
 client.run('ODc5NjI0OTcwNDkyMzI1OTM4.YSSclw.H9-hcHYoJOHFZOxln4485_Tlkgc')
