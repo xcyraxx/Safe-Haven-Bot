ef58c743ad5c8223d0c6771729f3f8bc1103c867
AdilJafar <64605907+xcyraxx@users.noreply.github.com>
2021-09-12 16:02:38 +0530

    Add files via upload

---
+++ cogs/reminder.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Cog
+import asyncio
+
+class Reminder(commands.Cog):
+    def __init__(self, client):
+        self.client=client
+
+    @Cog.listener()
+    async def on_ready(self):
+        print('Reminder up!')
+
+    @commands.command()
+    async def reminder(self, ctx, time: int, *, reason=None):
+        await ctx.send(f"I'll ping you in {time} hour(s)")
+        hour = time*2600
+        print(hour)
+        await asyncio.sleep(int(time))
+        await ctx.send(f"{ctx.author.mention}. {reason}")
+
+
+def setup(bot):
+    bot.add_cog(Reminder(bot))
